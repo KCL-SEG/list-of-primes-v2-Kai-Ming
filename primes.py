68b7af741ea25c40ccba56a697c8a739aa405529
Kai Ming Tey <kai.tey@kcl.ac.uk>
2022-10-03 01:39:32 +0100

    Prime v2 first attempt

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError
     list = []
+    pointer = 1
+    counter = 0
+    while counter < number_of_primes:
+        pointer += 1
+        is_prime = True
+        for num in list:
+            if pointer % num != 0:
+                is_prime = True
+            else:
+                is_prime = False
+                break
+        if is_prime:
+            list.append(pointer)
+            counter += 1
     return list
